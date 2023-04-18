da1d3e301d2bbe80ec42fa6abb96d5175c653e1b
Gairik Sharma <gairiksharma202@gmail.com>
2023-04-18 20:40:37 +0530

    First Commit

--- src/chat.js
+++ src/chat.js
@@ -1,19 +1,24 @@
-import React, { useState, useEffect, useRef } from 'react'
-import { db } from './firebase'
-import { collection } from 'firebase/firestore'
-import { orderBy, query, onSnapshot } from 'firebase/firestore'
-import auth from './firebase'
-import SendMessage from './sendmsg'
-import SignOut from './signOut'
-import './chat.css'
-import { deleteMessage } from './sendmsg'
-import { AiOutlineClose } from 'react-icons/ai';
+import React, { useState, useEffect, useRef } from "react";
+import { db } from "./firebase";
+import { collection } from "firebase/firestore";
+import { orderBy, query, onSnapshot } from "firebase/firestore";
+import auth from "./firebase";
+import SendMessage from "./sendmsg";
+import SignOut from "./signOut";
+import "./chat.css";
+import { deleteMessage } from "./sendmsg";
+import { AiOutlineClose } from "react-icons/ai";
 
+// import { storage } from "./firebase";
+// import { ref, uploadBytes, listAll, getDownloadURL } from "firebase/storage";
+// import { v4 } from "uuid";
 
-function Chat() {
-  const scroll = useRef()
-  const [messages, setMessages] = useState([])
-  const { photoURL } = auth.currentUser
+function Chat(props) {
+  // console.log(props.imageHandler);
+
+  const scroll = useRef();
+  const [messages, setMessages] = useState([]);
+  const { photoURL } = auth.currentUser;
   useEffect(() => {
     const collectionRef = collection(db, "messages");
     const q = query(collectionRef, orderBy("time"));
@@ -25,63 +30,144 @@ function Chat() {
       setMessages(arr);
       // console.log("loaded man");
     });
-  }, [])
+  }, []);
   useEffect(() => {
-    scroll.current.scrollIntoView({ behaviour: 'smooth' })
-  }, [messages])
-  const [signout, setSignout] = useState(false)
-  const showSignout = () => {
-    setSignout(true)
-    console.log('clicked');
-  }
-  const closeSignout = () => {
-    setSignout(false)
-  }
-  const currentUserName = auth.currentUser.displayName
+    scroll.current.scrollIntoView({ behaviour: "smooth" });
+  }, [messages]);
+  // const [signout, setSignout] = useState(false);
+  // const showSignout = () => {
+  //   setSignout(true);
+  //   console.log("clicked");
+  // };
+  // const closeSignout = () => {
+  //   setSignout(false);
+  // };
+  const currentUserName = auth.currentUser.displayName;
+
+  // const [file, setFile] = useState(null);
+  // const fileUpload = (e) => {
+  //   e.preventDefault();
+  //   if (file === null) return;
+
+  //   const imageRef = ref(storage, `images/${file.name + v4()}`);
+  //   uploadBytes(imageRef, file).then(() => {
+  //     alert("Image Uploaded");
+  //   });
+  // };
+  // const imageListRef = ref(storage, "images/");
+  // const [imglist, setImglist] = useState([]);
+  // useEffect(() => {
+  //   listAll(imageListRef).then((res) => {
+  //     res.items.forEach((item) => {
+  //       getDownloadURL(item).then((url) => {
+  //         setImglist((prev) => [...prev, url]);
+  //         console.log(url);
+  //       });
+  //     });
+  //   });
+  // }, []);
 
   return (
     <>
-      <nav>
-        <div className='logo'>
-          <div><span style={{ color: 'black' }}>C</span>hat<span style={{ color: 'black' }}>X</span></div>
-        </div>
-
-        <img src={auth.currentUser.photoURL} alt="" className='profile' onClick={showSignout} />
-        {
-          signout && (
-            <div className="showSignOut">
-              <AiOutlineClose className='close' onClick={closeSignout} />
-              <img src={auth.currentUser.photoURL} alt="" className='profile' onClick={showSignout} />
-              <h3 style={{ color: 'black', textAlign: 'center' }}>{currentUserName}</h3>
-              <SignOut className='signout-btn'/>
-            </div>
-          )
-        }
-      </nav>
-      <div className="container">
-        {
-          messages.map(({ id, text, photoURL, uid, displayName, time }) => {
-
-            return <div key={id} className='text-with-photo'>
-
-              <div className={`wraper ${uid === auth.currentUser.uid && 'wraper-sent' }`}>
-                  <img src={photoURL} alt="" />
-                <div key={id} className={`${uid === auth.currentUser.uid ? 'sent' : 'recived'}`}>
-                  <div>
-                    <div className='display-name'>{displayName}</div>
-                    <p>{text}</p>
-                  </div>
-                </div>
+      <div className="app-wrapper">
+        <div className="app">
+          <nav>
+            <div className="logo">
+              <div>
+                <span style={{ color: "black" }}>C</span>hat
+                <span style={{ color: "black" }}>X</span>
               </div>
             </div>
-          })
-        }
+
+            <div className="profile-signout-btn">
+              <div className="nav-profile">
+                <img
+                  src={auth.currentUser.photoURL}
+                  alt=""
+                  className="profile"
+                  // onClick={showSignout}
+                />
+              </div>
+              <SignOut className="signout-btn" />
+            </div>
+            {/* {signout && (
+              <div className="showSignOut">
+                <AiOutlineClose className="close" onClick={closeSignout} />
+                <img
+                  src={auth.currentUser.photoURL}
+                  alt=""
+                  className="profile"
+                  onClick={showSignout}
+                />
+                <h3 style={{ color: "black", textAlign: "center" }}>
+                  {currentUserName}
+                </h3>
+                <SignOut className="signout-btn" />
+              </div>
+            )} */}
+
+            {/* <input
+          type="file"
+          onChange={(event) => {
+            setFile(event.target.files[0]);
+          }}
+        />
+        <button onClick={fileUpload}>File</button> */}
+          </nav>
+
+          <div className="container">
+            {messages.map(({ id, text, photoURL, uid, displayName, time }) => {
+              // console.log('g');
+              // if(text == " "){
+              //   alert('please enter some text')
+              // }
+
+              return (
+                <div key={id} className="text-with-photo">
+                  {/* {imglist.map((url) => {
+                return (
+                  <div className="image-wrapper">
+                    <img className="send-img" src={url} alt="" />
+                  </div>
+                );
+              })} */}
+                  <div
+                    className={`wraper ${
+                      uid === auth.currentUser.uid && "wraper-sent"
+                    }`}
+                  >
+                    <img src={photoURL} alt="" />
+                    <div
+                      key={id}
+                      className={`${
+                        uid === auth.currentUser.uid ? "sent" : "recived"
+                      }`}
+                    >
+                      <div>
+                        <div className="display-name">{displayName}</div>
+                        <span className="text-message">{text}</span>
+                      </div>
+                      {/* {imglist.map((url) => {
+                    return (
+                      <div className="image-wrapper">
+                        <img className="send-img" src={url} alt="" />
+                      </div>
+                    );
+                  })} */}
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+
+          <div ref={scroll} className="scroll"></div>
+
+          <SendMessage />
+        </div>
       </div>
-      <div ref={scroll} className='scroll'></div>
-      <SendMessage />
-      
     </>
-  )
+  );
 }
 
-export default Chat
+export default Chat;
--- src/sendmsg.js
+++ src/sendmsg.js
@@ -1,53 +1,109 @@
-import { async } from '@firebase/util'
-import React, { useState, useRef } from 'react'
-import { addDoc, collection } from 'firebase/firestore'
-import { db } from './firebase'
-import auth from './firebase'
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
-import './chat.css'
+import React, { useState, useEffect } from "react";
+import { addDoc, collection } from "firebase/firestore";
+import { db } from "./firebase";
+import auth from "./firebase";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+import "./chat.css";
+
+
+import { storage } from "./firebase";
+import { ref, uploadBytes, listAll, getDownloadURL } from "firebase/storage";
+import { v4 } from "uuid";
 
 
 function SendMessage() {
-    // const end = useRef()
-    const [msg, setMsg] = useState('')
-    async function sendMessage(e) {
-        if (msg==='') {
-            console.log('enter some text');
-        } else {
-            e.preventDefault()
-            const { uid, photoURL, displayName } = auth.currentUser
-            const collectionRef = collection(db, 'messages');
-            const payload = {
-                text: msg,
-                uid,
-                photoURL,
-                time: firebase.firestore.FieldValue.serverTimestamp(),
-                displayName
-            }
-            console.log(payload);
-            await addDoc(collectionRef, payload)
-            setMsg('')
-        }
-        
+
+  // const [file, setFile] = useState(null);
+  // const fileUpload = (e) => {
+  //   e.preventDefault();
+  //   if (file === null) return;
+
+  //   const imageRef = ref(storage, `images/${file.name + v4()}`);
+  //   uploadBytes(imageRef, file).then(() => {
+  //     alert("Image Uploaded");
+  //   });
+  // };
+
+
+  // const imageListRef = ref(storage, "images/");
+  // const [imglist, setImglist] = useState([]);
+  // useEffect(() => {
+  //   listAll(imageListRef).then((res) => {
+  //     res.items.map((item) => {
+  //       getDownloadURL(item).then((url) => {
+  //         setImglist((prev) => [...prev, url]);
+  //       });
+  //     });
+  //   });
+  // }, []);
+
+  
+  const [msg, setMsg] = useState("");
+  async function sendMessage(e) {
+    if (msg === "") {
+      alert("enter some text");
+    } else {
+      e.preventDefault();
+      const { uid, photoURL, displayName } = auth.currentUser;
+      const collectionRef = collection(db, "messages");
+      const payload = {
+        text: msg,
+        uid,
+        photoURL,
+        time: firebase.firestore.FieldValue.serverTimestamp(),
+        displayName,
+      };
+      console.log(payload);
+      await addDoc(collectionRef, payload);
+      setMsg("");
     }
-    return (
-        <>
-            <div>
-                <form onSubmit={sendMessage}>
-                    
-                        <input type="text" value={msg} placeholder='Send message...' onChange={(e) => {
-                            setMsg(e.target.value)
-                        }} />
-                        <button type='submit' className='send-button'>
-                            <p>send</p>
-                        </button>
-                    
-                </form>
-            </div>
-        </>
-    )
+  }
+
+  // const [file, setFile] = useState(null);
+  // const fileUpload = (e) => {
+  //   e.preventDefault();
+  //   if (file === null) return;
+
+  //   const imageRef = ref(storage, `images/${file.name + v4()}`);
+  //   uploadBytes(imageRef, file).then(() => {
+  //     alert("Image Uploaded");
+  //   });
+  // };
+
+  // const imageListRef = ref(storage, "images/");
+  // const [imglist, setImglist] = useState([]);
+  // useEffect(() => {
+  //   listAll(imageListRef).then((res) => {
+  //     res.items.map((item) => {
+  //       getDownloadURL(item).then((url) => {
+  //         setImglist((prev) => [...prev, url]);
+  //       });
+  //     });
+  //   });
+  // }, []);
+
+  return (
+    <>
+      
+      <div>
+        <form onSubmit={sendMessage} className="send-message">
+          <input
+            type="text"
+            value={msg}
+            placeholder="Send message..."
+            onChange={(e) => {
+              setMsg(e.target.value);
+            }}
+          />
+          <button type="submit" className="send-button">
+            <p>send</p>
+          </button>
+        </form>
+      </div>
+    </>
+  );
 }
 
-export default SendMessage
+export default SendMessage;
